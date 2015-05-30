b7b9ca3a670095964885f3fb2aad22ff08d8ccd0
Amandjz420 <amanagrwl@yahoo.com>
2015-05-30 14:35:36 +0530

    phantomjs

---
+++ value.js
@@ -0,0 +1,38 @@
+var page = require('webpage').create();
+console.log('page created');
+page.open('http://www.carwale.com/used/carvaluation/',function() {
+	console.log('page fetched');
+	//page.injectJs("jquery-1.9.1.min.js", function() {
+		//console.log('found jquery');
+		page.evaluate(function() {
+			$('#rdoValType_1').prop( "checked", true );
+			$('#cmbMonth').val(2).change();
+			$('#cmbYear').val(2012).change();
+			var interval = setInterval(function(){
+			if($('#cmbMake').attr('disabled') === 'disabled') {
+				phantom.exit(1)
+			}else{
+				$('#cmbMake').val(2012).change();
+				$('#cmbMake').val(7).change();
+				$('#cmbMMake').on('change',function(){
+					$('#cmbModel').val(215).change();
+					$('#cmbModel').on('change',function(){
+							$('#cmbVersion').val(2639).change();
+							clearInterval(interval);
+							});
+						});
+					}
+			
+				});
+			$('#txtKms').val(44033).change();
+			$('#cmbValuationCity').val(1).change();
+
+			$('#form1').submit();
+					
+			
+			});
+			
+			setTimeout( page.render('example.png'), 3000);
+			}//console.log('here on page');
+			phantom.exit()
+});
