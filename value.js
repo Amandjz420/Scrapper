f330ace57176d258670e0b182b231beabf72c0c6
Amandjz420 <amanagrwl@yahoo.com>
2015-06-03 04:01:34 +0530

    Revert "phantomjs"
    This reverts commit b7b9ca3a670095964885f3fb2aad22ff08d8ccd0.
    
    Conflicts:
    	value.js

--- value.js
+++ value.js
@@ -1,38 +1,98 @@
 var page = require('webpage').create();
-console.log('page created');
-page.open('http://www.carwale.com/used/carvaluation/',function() {
-	console.log('page fetched');
-	//page.injectJs("jquery-1.9.1.min.js", function() {
-		//console.log('found jquery');
-		page.evaluate(function() {
-			$('#rdoValType_1').prop( "checked", true );
-			$('#cmbMonth').val(2).change();
-			$('#cmbYear').val(2012).change();
-			var interval = setInterval(function(){
-			if($('#cmbMake').attr('disabled') === 'disabled') {
-				phantom.exit(1)
-			}else{
-				$('#cmbMake').val(2012).change();
-				$('#cmbMake').val(7).change();
-				$('#cmbMake').on('change',function(){
-					setTimeout($('#cmbModel').val(215).change(),1000);
-					$('#cmbModel').on('change',function(){
-							setTimeout($('#cmbVersion').val(2639).change(),1000);
-							clearInterval(interval);
-							});
-						});
-					}
-			
-				});
-			$('#txtKms').val(44033).change();
-			$('#cmbValuationCity').val(1).change();
 
-			$('#form1').submit();
-					
-			
-			});
-			
-			setTimeout( page.render('example.png'), 3000);
-			}//console.log('here on page');
-			phantom.exit()
+console.log('page created');
+page.onConsoleMessage = function (msg){
+	console.log(msg);
+};
+page.open('http://www.carwale.com/used/carvaluation/',function (i){
+    
+	console.log('page fetched');
+	page.evaluate(function (t){
+        //function for selecting version to be called in function model select 
+        //better to go below first
+        function versionselect(){
+            var versioncheck = setInterval(function (){
+                if( $('#cmbVersion').prop('disabled') == false ) {
+                    $('#cmbVersion').val(2620).change(); 
+                    console.log( "changing model and submitting form" );
+                    $('#txtKms').val(5333).change();
+                    $('#cmbValuationCity').val(1).change();
+   
+                   // console.log( $('#form1').serialize() );
+                    $('#btnSave').click();
+                    
+                    clearInterval(versioncheck);
+
+                }else{
+                    console.log('Waitng for version list');
+                }
+
+            },1000);
+
+        }
+        //function for selecting model to be called in function make select
+        //keep going down 
+        function modelselect(){
+            var modelcheck = setInterval(function (){
+                if( $('#cmbModel').prop('disabled') == false ) {
+                    $('#cmbModel').val(1).change(); 
+                    console.log( "changing model" );
+                    versionselect();
+                    clearInterval(modelcheck);
+
+                }else{
+                    console.log('Waitng for model list');
+                }
+
+            },1500);
+
+        }
+        //stop now start seeing the code
+		//entering enabled fields
+        console.log("entering");
+
+		$('#rdoValType_1').prop("checked" , true);
+		$('#cmbMonth').val(2).change();
+		$('#cmbYear').val(2012).change();
+
+        //selecting the value of make and calling model function
+        var tot = setInterval(function () {
+            if( $('#cmbMake').prop('disabled') == false ) {
+                $('#cmbMake').val(1).change();
+                console.log('making change');
+                modelselect();
+                
+                clearInterval(tot);
+            } else {
+                console.log("waiting for make") ;
+            }
+        }, 2000);
+        
+    });
+    //loop running with the above loop n will stop 
+    //after submitting the form by checking the title of page
+    //because global variable were still not working :(
+    setInterval(function (){
+      var title = page.evaluate(function() {
+        return document.title;
+        });
+    console.log(title);
+    if( title == "CarValue | Used | CarWale CarValue - CarWale" ){
+        page.evaluate(function(){
+            //getting the values :).....finally after 1 week
+            console.log($("#lblFairDealer").text() );
+            console.log($("#lblGoodDealer").text() );
+            console.log($("#lblExcellentDealer").text() );
+            console.log($("#lblFair").text() );
+            console.log($("#lblGood").text() );
+            console.log($("#lblExcellent").text() );
+            
+        });     
+        // rendering just for fun then exit 
+        page.render("./useless/example3.png");
+        console.log("exiting");
+        phantom.exit();    
+               };
+    },3000);     
+
 });
